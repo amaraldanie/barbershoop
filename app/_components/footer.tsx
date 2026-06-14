fb862bd79d2d9ae4977b059a23aa2c9c983b4ebd
Daniel <danieldo.amaral0601@gmail.com>
2026-06-14 11:13:51 -0300

    style(ui): auto update

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -3,13 +3,15 @@ import { Card, CardContent } from "./ui/card"
 const Footer = () => {
   return (
     <footer className="mt-10 flex w-full justify-center">
-      <Card className="w-full max-w-6xl">
-        <CardContent className="py-6">
-          <p className="text-center text-sm text-gray-400">
-            © 2026 Copyright <span className="font-bold">Movo Barber</span>
-          </p>
-        </CardContent>
-      </Card>
+      <div className="w-full max-w-6xl">
+        <Card>
+          <CardContent className="flex justify-center py-6">
+            <p className="w-full text-center text-sm text-gray-400">
+              © 2026 Copyright <span className="font-bold">Movo Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </div>
     </footer>
   )
 }
